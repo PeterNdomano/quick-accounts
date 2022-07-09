07f56412b7965418cd8c293ea57533fec5847cda
PeterNdomano <peterndomano@gmail.com>
2022-07-09 13:59:48 +0300

    proxy, misc user management fron-end

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { checkLogin } from './Helper';
+import Login from './components/Login';
 
 function App() {
+  const [ loginStatus, setLoginStatus ] = useState(0);
+
+  useState(() => {
+    //re-render
+    console.log(loginStatus);
+  }, [loginStatus]);
+
+  let getLoginStatus = async () => {
+    await checkLogin().then(response => {
+      //set login status
+      setLoginStatus(response.status);
+    }, error => {
+      //error occured
+    });
+  }
+
+
   return (
     <div className="App">
-      
+      {
+        (loginStatus === 1) ?
+        <>
+          <h1>Logged In</h1>
+          <h1>Logged In</h1>
+        </>
+        :
+        <>
+          <Login/>
+        </>
+      }
     </div>
   );
 }
--- ui/src/Helper.js
+++ ui/src/Helper.js
@@ -1,10 +1,21 @@
 import $ from 'jquery';
 
-export const MAIN_URL = 'http://localhost/github_projects/quick-accounts/api/';
-export async function checkLogin(){
-
+export const MAIN_URL = 'github_projects/quick-accounts/api/';
+export function checkLogin(){
+  return new Promise((resolved, rejected) => {
+    $.post(MAIN_URL+'check_login.php', {}, (data, status) => {
+      if(status === 'success') {
+        let response = JSON.parse(data);
+        //this is exception, here 0 means not logged in and 1 means logged in
+        resolved(response);
+      }
+      else {
+        rejected("Network error");
+      }
+    });
+  });
 }
 
-export function checkUserRole(){
+export async function checkUserRole(){
 
 }
