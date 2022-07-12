780d9ce165084877db0cf14107629419768645d4
PeterNdomano <peterndomano@gmail.com>
2022-07-12 12:42:34 +0300

    open and close menu

--- ui/src/components/Nav.js
+++ ui/src/components/Nav.js
@@ -3,10 +3,29 @@ import { MdMenu } from 'react-icons/md';
 
 
 export default function Main(){
+
+  let openMenu = () => {
+    document.getElementsByClassName('topBar')[0].dataset['menu'] = 'opened';
+    document.getElementsByClassName('navMenu')[0].dataset['menu'] = 'opened';
+    document.getElementsByClassName('MainBody')[0].dataset['menu'] = 'opened';
+  }
+
+  let closeMenu = () => {
+    document.getElementsByClassName('topBar')[0].dataset['menu'] = 'closed';
+    document.getElementsByClassName('navMenu')[0].dataset['menu'] = 'closed';
+    document.getElementsByClassName('MainBody')[0].dataset['menu'] = 'closed';
+  }
   let toggleMenu = () => {
     let ind = document.getElementsByClassName('topBar')[0].dataset['menu'];
-    if(ind === 'default' || ind === '') {
-
+    if(ind === 'closed') {
+      openMenu();
+    }
+    else if (ind === 'default') {
+      //open on mobile, close on desktop
+      openMenu(); //temporary
+    }
+    else {
+      closeMenu();
     }
   }
   return (
