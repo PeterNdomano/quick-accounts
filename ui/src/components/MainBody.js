750eb44efe416e4c5c22a4cf0bb076f887a12286
PeterNdomano <peterndomano@gmail.com>
2022-07-12 11:54:43 +0300

    nav menu designs

--- ui/src/components/MainBody.js
+++ ui/src/components/MainBody.js
@@ -3,7 +3,9 @@ import React, { useState } from 'react';
 export default function Main(){
   return (
     <div className="MainBody">
+      <div className="container bg-danger" style={{ height:"300px" }}>
 
+      </div>
     </div>
   )
 }
