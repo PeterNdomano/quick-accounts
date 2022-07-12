e680b28077a85bb27429c01f75a7fb62a1897737
PeterNdomano <peterndomano@gmail.com>
2022-07-12 12:35:47 +0300

    menu designs

--- ui/src/components/MainBody.js
+++ ui/src/components/MainBody.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export default function Main(){
   return (
-    <div className="MainBody">
+    <div className="MainBody" data-menu="default">
       <div className="container bg-danger" style={{ height:"300px" }}>
 
       </div>
--- ui/src/components/Nav.js
+++ ui/src/components/Nav.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import { MdMenu } from 'react-icons/md';
 
+
 export default function Main(){
+  let toggleMenu = () => {
+    let ind = document.getElementsByClassName('topBar')[0].dataset['menu'];
+    if(ind === 'default' || ind === '') {
+
+    }
+  }
   return (
     <div className="Nav">
-      <div className="topBar d-flex">
-        <MdMenu className="menuIcon align-self-center"/>
+      <div className="topBar d-flex" data-menu="default">
+        <MdMenu onClick={toggleMenu} className="menuIcon align-self-center"/>
         <h3 className="topTitle align-self-center">Quick Accounts</h3>
       </div>
-      <div className="navMenu">
+      <div className="navMenu" data-menu="default">
       </div>
     </div>
   )
