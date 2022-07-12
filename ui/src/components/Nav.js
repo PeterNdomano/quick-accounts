bed4de31cc0322556ebf4ca3cdd7ec49359bd658
PeterNdomano <peterndomano@gmail.com>
2022-07-12 11:37:04 +0300

    top bar designs

--- ui/src/components/Nav.js
+++ ui/src/components/Nav.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import { MdMenu } from 'react-icons/md';
 
 export default function Main(){
   return (
     <div className="Nav">
-      <div className="topBar">
-
+      <div className="topBar d-flex">
+        <MdMenu className="menuIcon align-self-center"/>
+        <h3 className="topTitle align-self-center">Quick Accounts</h3>
       </div>
-      <div className="navMenu"></div>
+      <div className="navMenu">
+      </div>
     </div>
   )
 }
