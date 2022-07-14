23d2f604b1bc59b5a484283bc0352bf5052ab91c
PeterNdomano <peterndomano@gmail.com>
2022-07-14 10:05:25 +0300

    navigation completion

--- ui/src/Helper.js
+++ ui/src/Helper.js
@@ -1,6 +1,13 @@
 import $ from 'jquery';
 import { toast } from 'react-toastify';
 import { TailSpin } from 'react-loader-spinner';
+import Income from './body-divs/Income';
+import Expenses from './body-divs/Expenses';
+import Receivables from './body-divs/Receivables';
+import Payables from './body-divs/Payables';
+import Payroll from './body-divs/Payroll';
+import Reports from './body-divs/Reports';
+import Settings from './body-divs/Settings';
 
 export const MAIN_URL = 'github_projects/quick-accounts/api/';
 export function checkLogin(){
@@ -88,5 +95,27 @@ export function isMobile() {
   }
   else {
     return false;
+  }
+}
+
+export function getBodyDiv(navItem) {
+  switch (navItem) {
+    case "navIncome":
+      return <Income/>;
+    case "navExpenses":
+      return <Expenses/>;
+    case "navPayables":
+      return <Payables/>;
+    case "navReceivables":
+      return <Receivables/>;
+    case "navPayroll":
+      return <Payroll/>;
+    case "navReports":
+      return <Reports/>;
+    case "navSettings":
+      return <Settings/>;
+    default:
+      return <Income/>;
+
   }
 }
