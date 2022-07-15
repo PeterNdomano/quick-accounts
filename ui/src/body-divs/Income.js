bb09d65376e3708ca6a7754480bb40bbc091a9e8
PeterNdomano <peterndomano@gmail.com>
2022-07-15 14:56:17 +0300

    basic body div design

--- ui/src/body-divs/Income.js
+++ ui/src/body-divs/Income.js
@@ -1,11 +1,62 @@
 import React, { useState, useContext } from 'react';
-
+import { MainContext } from '../App';
+import { MdAdd } from 'react-icons/md';
 
 export default function Component() {
+
+  const mainContext = useContext(MainContext);
+  const QA = mainContext.QA;
+
+
   return (
     <div className="container">
       <div className="row">
-        <h1>Income</h1>
+
+        <div className="col-md-6 col-sm-12">
+          <div className="card introCard bg-success">
+            <div className="card-body d-flex">
+              <h4 className="align-self-center flex-grow-1" style={{ margin:"0" }}>
+                <span style={{ fontSize:"70px" }}>10 M</span>
+              </h4>
+              <h6 className="align-self-center" style={{ margin:"0" }}>
+                <span style={{ fontSize:"30px" }}>Tsh</span><br/>
+                <span style={{ fontSize:"12px" }}>Today's Income</span>
+              </h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-6 col-sm-12">
+          <div className="card introCard bg-warning">
+            <div className="card-body d-flex">
+              <h4 className="align-self-center flex-grow-1" style={{ margin:"0" }}>
+                <span style={{ fontSize:"70px" }}>10 M</span>
+              </h4>
+              <h6 className="align-self-center" style={{ margin:"0" }}>
+                <span style={{ fontSize:"30px" }}>Tsh</span><br/>
+                <span style={{ fontSize:"12px" }}>Total Income</span>
+              </h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-12 col-sm-12">
+          <div className="card">
+            <div className="card-body">
+
+              <div style={{ height:"30px" }} className="card-title d-flex justify-content-between">
+                <h6 className="align-self-center">Income Records</h6>
+                <button className="align-self-center btn btn-dark btn-sm text-light">
+                  <MdAdd size={12}/>&nbsp;&nbsp;Add New
+                </button>
+              </div>
+              <hr/>
+
+              
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   )
