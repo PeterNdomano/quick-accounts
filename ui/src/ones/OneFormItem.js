c261ad43d79740c8f7a2bfbcd6fb8a7cf7df8d5c
PeterNdomano <peterndomano@gmail.com>
2022-07-16 10:53:32 +0300

    One table row design

--- ui/src/ones/OneFormItem.js
+++ ui/src/ones/OneFormItem.js
@@ -3,9 +3,33 @@ import React from 'react';
 export default function Component() {
   return (
     <div className="row OneFormItem">
-      <div className="col-md-2 col-sm-12">
+      <div className="col-md-3 col-sm-12">
         <div className="form-group">
           <label>Particular</label>
+          <input className="form-control"/>
+        </div>
+      </div>
+      <div className="col-md-2 col-sm-12">
+        <div className="form-group">
+          <label>Unit</label>
+          <input className="form-control"/>
+        </div>
+      </div>
+      <div className="col-md-2 col-sm-12">
+        <div className="form-group">
+          <label>Quantity</label>
+          <input className="form-control"/>
+        </div>
+      </div>
+      <div className="col-md-2 col-sm-12">
+        <div className="form-group">
+          <label>Unit Price</label>
+          <input className="form-control"/>
+        </div>
+      </div>
+      <div className="col-md-3 col-sm-12">
+        <div className="form-group">
+          <label>Sub Total</label>
           <input className="form-control"/>
         </div>
       </div>
