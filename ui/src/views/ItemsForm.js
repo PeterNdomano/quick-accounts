f9ff13a9ed67a3d43f52d73b69df8c391df66bb9
PeterNdomano <peterndomano@gmail.com>
2022-07-17 22:13:10 +0300

    add row mechanism

--- ui/src/views/ItemsForm.js
+++ ui/src/views/ItemsForm.js
@@ -3,11 +3,10 @@ import OneFormItem from '../ones/OneFormItem';
 import { MdAdd } from 'react-icons/md';
 
 export default function Component(props) {
-  const [ rows, setRows ] = useState("peter");
-  const [ name, setName ] = useState("First name");
+  const [ rows, setRows ] = useState([<OneFormItem/>]);
 
   const addRow = () => {
-    setRows('Ndomano')
+    setRows([ ...rows, <OneFormItem/> ])
   }
 
   useEffect(() => {
@@ -17,10 +16,10 @@ export default function Component(props) {
   return (
     <div className="ItemsForm" id="itemsForm">
       <div id="tableRows" style={{ width:"100%", margin:"0px", padding:"0px" }}>
-        {name}
+        {rows}
       </div>
       <div className="text-right">
-        <button onClick={() => { setName('Second name') }} className="btn btn-sm btn-dark">
+        <button onClick={addRow} className="btn btn-sm btn-dark">
           <MdAdd size={12}/>&nbsp;&nbsp;Add Row
         </button>
       </div>
