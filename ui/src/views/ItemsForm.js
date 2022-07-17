0e6daf36c6c9b3e553615a21741278565fa4aca9
PeterNdomano <peterndomano@gmail.com>
2022-07-17 22:10:14 +0300

    mods

--- ui/src/views/ItemsForm.js
+++ ui/src/views/ItemsForm.js
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import OneFormItem from '../ones/OneFormItem';
+import { MdAdd } from 'react-icons/md';
 
 export default function Component(props) {
+  const [ rows, setRows ] = useState("peter");
+  const [ name, setName ] = useState("First name");
+
+  const addRow = () => {
+    setRows('Ndomano')
+  }
+
+  useEffect(() => {
+    console.log('rows changed');
+  }, [rows]);
+
   return (
-    <div className="ItemsForm card" id="itemsForm">
-      <div className="card-body">
-        <OneFormItem/>
+    <div className="ItemsForm" id="itemsForm">
+      <div id="tableRows" style={{ width:"100%", margin:"0px", padding:"0px" }}>
+        {name}
+      </div>
+      <div className="text-right">
+        <button onClick={() => { setName('Second name') }} className="btn btn-sm btn-dark">
+          <MdAdd size={12}/>&nbsp;&nbsp;Add Row
+        </button>
       </div>
     </div>
   )
--- ui/src/views/NewIncome.js
+++ ui/src/views/NewIncome.js
@@ -7,53 +7,52 @@ export default function Component(props) {
   }
   return (
     <div className="container">
-      <form style={{ width: "100%" }}>
-        <div className="row">
 
-          <div className="col-md-12 col-sm-12">
-            <h4>Basic Information</h4>
-          </div>
-          <div className="col-md-6 col-sm-12">
-            <div className="form-group">
-              <label>Title</label>
-              <input className="form-control" type="text"/>
-            </div>
-            <div className="form-group">
-              <label>Description (Optional)</label>
-              <textarea className="form-control"></textarea>
-            </div>
-            <div className="form-group">
-              <label>Date</label>
-              <input className="form-control" type="date"/>
-            </div>
-          </div>
+      <div className="row">
 
-          <div className="col-md-6 col-sm-12">
-            <div className="form-group">
-              <label>Total Amount To be Received</label>
-              <input className="form-control" type="number"/>
-            </div>
-            <div className="form-group">
-              <label>Amount Currently Received</label>
-              <input className="form-control" type="number"/>
-            </div>
-            <div className="form-group">
-              <h6>
-                An account will be created to track payments if amount received is less
-                than total amount to be received
-              </h6>
-            </div>
+        <div className="col-md-12 col-sm-12">
+          <h4>Basic Information</h4>
+        </div>
+        <div className="col-md-6 col-sm-12">
+          <div className="form-group">
+            <label>Title</label>
+            <input className="form-control" type="text"/>
           </div>
-
-          <div className="col-md-12 col-sm-12">
-            <h4>Particulars</h4>
-            <h6>*Only use this part if you want to specify items for invoice creation otherwise it can be ignored</h6>
+          <div className="form-group">
+            <label>Description (Optional)</label>
+            <textarea className="form-control"></textarea>
           </div>
-          <div className="col-md-12 col-sm-12">
-            <ItemsForm reporter={itemsFormReporter} />
+          <div className="form-group">
+            <label>Date</label>
+            <input className="form-control" type="date"/>
           </div>
         </div>
-      </form>
+
+        <div className="col-md-6 col-sm-12">
+          <div className="form-group">
+            <label>Total Amount To be Received</label>
+            <input className="form-control" type="number"/>
+          </div>
+          <div className="form-group">
+            <label>Amount Currently Received</label>
+            <input className="form-control" type="number"/>
+          </div>
+          <div className="form-group">
+            <h6>
+              An account will be created to track payments if amount received is less
+              than total amount to be received
+            </h6>
+          </div>
+        </div>
+
+        <div className="col-md-12 col-sm-12">
+          <h4>Particulars</h4>
+          <h6>*Only use this part if you want to specify items for invoice creation otherwise it can be ignored</h6>
+        </div>
+        <div className="col-md-12 col-sm-12">
+          <ItemsForm reporter={itemsFormReporter} />
+        </div>
+      </div>
     </div>
   );
 }
