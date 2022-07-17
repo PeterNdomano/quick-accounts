a97c99e0023ca0f4be58b50257e298176e8b6b3c
PeterNdomano <peterndomano@gmail.com>
2022-07-17 22:27:46 +0300

    delete row mechanism

--- ui/src/ones/OneFormItem.js
+++ ui/src/ones/OneFormItem.js
@@ -1,42 +1,60 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
+import { MdDelete } from 'react-icons/md';
 
 export default function Component() {
+
+  const [ deleted, setDeleted ] = useState(false);
   return (
-    <div className="OneFormItem card">
-      <div className="card-body">
-        <div className="row OneFormItem">
-          <div className="col-md-3 col-sm-12">
-            <div className="form-group">
-              <label>Particular</label>
-              <input className="form-control"/>
+    <>
+    {
+      (!deleted)
+      ?
+      <div className="OneFormItem card">
+        <div className="card-body">
+          <div className="row OneFormItem">
+            <div className="col-md-3 col-sm-12">
+              <div className="form-group">
+                <label>Particular</label>
+                <input className="form-control"/>
+              </div>
             </div>
-          </div>
-          <div className="col-md-2 col-sm-12">
-            <div className="form-group">
-              <label>Unit</label>
-              <input className="form-control"/>
+            <div className="col-md-2 col-sm-12">
+              <div className="form-group">
+                <label>Unit</label>
+                <input className="form-control"/>
+              </div>
             </div>
-          </div>
-          <div className="col-md-2 col-sm-12">
-            <div className="form-group">
-              <label>Quantity</label>
-              <input className="form-control"/>
+            <div className="col-md-2 col-sm-12">
+              <div className="form-group">
+                <label>Quantity</label>
+                <input className="form-control"/>
+              </div>
             </div>
-          </div>
-          <div className="col-md-2 col-sm-12">
-            <div className="form-group">
-              <label>Unit Price</label>
-              <input className="form-control"/>
+            <div className="col-md-2 col-sm-12">
+              <div className="form-group">
+                <label>Unit Price</label>
+                <input className="form-control"/>
+              </div>
             </div>
-          </div>
-          <div className="col-md-3 col-sm-12">
-            <div className="form-group">
-              <label>Sub Total</label>
-              <input className="form-control"/>
+            <div className="col-md-2 col-sm-12">
+              <div className="form-group">
+                <label>Sub Total</label>
+                <input className="form-control"/>
+              </div>
+            </div>
+            <div className="col-md-1 col-sm-12">
+              <div className="form-group">
+                <MdDelete onClick={() => { setDeleted(true) }} style={{ cursor:"pointer", color:"var(--danger)", marginTop:"40px" }} size={20}/>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      :
+      <></>
+
+    }
+    </>
+
   )
 }
